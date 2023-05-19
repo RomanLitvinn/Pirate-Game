3fd7707c34ea63bd4ad0c9d6d35ab00150dc19fc
Roman <romalitvin7777@gmail.com>
2023-05-19 13:31:56 +0200

    Created menu and levels.

--- code/tiles.py
+++ code/tiles.py
@@ -1,5 +1,4 @@
 import pygame
-
 from support import import_folder
 
 
@@ -9,19 +8,19 @@ class Tile(pygame.sprite.Sprite):
         self.image = pygame.Surface((size, size))
         self.rect = self.image.get_rect(topleft=(x, y))
 
-    def update(self, x_shift):
-        self.rect.x += x_shift
+    def update(self, shift):
+        self.rect.x += shift
 
 
 class StaticTile(Tile):
-    def __init__(self, size, x, y, surf):
+    def __init__(self, size, x, y, surface):
         super().__init__(size, x, y)
-        self.image = surf
+        self.image = surface
 
 
 class Crate(StaticTile):
     def __init__(self, size, x, y):
-        super().__init__(size, x, y, pygame.image.load("../graphics/terrain/crate.png").convert_alpha())
+        super().__init__(size, x, y, pygame.image.load('../graphics/terrain/crate.png').convert_alpha())
         offset_y = y + size
         self.rect = self.image.get_rect(bottomleft=(x, offset_y))
 
@@ -31,32 +30,29 @@ class AnimatedTile(Tile):
         super().__init__(size, x, y)
         self.frames = import_folder(path)
         self.frame_index = 0
-        self.animation_speed = 0.15
         self.image = self.frames[self.frame_index]
 
     def animate(self):
-        self.frame_index += self.animation_speed
-
+        self.frame_index += 0.15
         if self.frame_index >= len(self.frames):
             self.frame_index = 0
-        else:
-            self.image = self.frames[int(self.frame_index)]
+        self.image = self.frames[int(self.frame_index)]
 
-    def update(self, x_shift):
+    def update(self, shift):
         self.animate()
-        self.rect.x += x_shift
+        self.rect.x += shift
 
 
-class Coins(AnimatedTile):
+class Coin(AnimatedTile):
     def __init__(self, size, x, y, path):
         super().__init__(size, x, y, path)
-        self.animation_speed = 0.08
-        self.rect = self.image.get_rect(center=(x+size/2, y+size/2))
+        center_x = x + int(size / 2)
+        center_y = y + int(size / 2)
+        self.rect = self.image.get_rect(center=(center_x, center_y))
 
 
 class Palm(AnimatedTile):
     def __init__(self, size, x, y, path, offset):
         super().__init__(size, x, y, path)
-        self.animation_speed = 0.1
         offset_y = y - offset
         self.rect.topleft = (x, offset_y)
